cd10d2adb6849b9c1567da386483bf36f39056fc
Marcello De Chirico <marcello.dechirico@atlantica.it>
2022-11-25 14:48:02 +0100

    service login & addUtente

---
+++ algoDemoBackend/src/main/java/com/example/demo/Service/ServiceUtente.java
@@ -0,0 +1,46 @@
+package com.example.demo.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Errore;
+import com.example.demo.Model.Utente;
+import com.example.demo.Model.UtenteLogin;
+import com.example.demo.Repository.UtenteRepository;
+
+@Service
+public class ServiceUtente {
+	@Autowired
+	private UtenteRepository utenteRepository;
+	
+	 public Utente checkLogin(UtenteLogin user) {
+		 Optional<Utente> ut=utenteRepository.findByEmail(user.getEmail());
+		 if (ut.isPresent()) {
+		    	Utente utn = ut.get();
+		    	if(utn.getPassword().equals(user.getPassword())) {
+		    		return utn;
+		    	}
+		 }
+		 return null;
+	 }
+	 
+	 public Utente addUser(Utente user) {
+		 Optional<Utente> ut=utenteRepository.findByCodiceFiscale(user.getCodFiscale());
+		  if(ut.isPresent()) {
+			  return null;
+		  }
+		  ut=utenteRepository.findByEmail(user.getEmail());
+		  if(ut.isPresent()) {
+			  return null;
+		  }
+		  utenteRepository.save(user);
+		  return user;
+	 }
+	 
+	 
+	 
+}
