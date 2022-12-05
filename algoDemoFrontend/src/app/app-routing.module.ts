be649f918028130762d07d3d9a9ca7d4b62d87be
ghisson <simo.fortu@yahoo.it>
2022-12-05 15:25:59 +0100

    add login page

--- algoDemoFrontend/src/app/app-routing.module.ts
+++ algoDemoFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaLoginComponent } from './ca-login/ca-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: CaLoginComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ algoDemoFrontend/src/app/ca-login/ca-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ca-login',
+  templateUrl: './ca-login.component.html',
+  styleUrls: ['./ca-login.component.css']
+})
+export class CaLoginComponent {
+
+}
